88190afa230bbf303c9c158104554bbf7628d7cc
Magnus Westin <magnus.westin@xlent.se>
2018-08-29 07:42:30 +0200

    First commit

---
+++ rest-test/src/main/java/se/mawe75/rest/MyService.java
@@ -0,0 +1,26 @@
+package se.mawe75.rest;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/myservice")
+public class MyService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String message() {
+		JsonObject object = Json.createObjectBuilder().add("name", "Kalle").add("age", 42).build();
+
+		return object.toString();
+	}
+
+	@POST
+	public String lowerCase(final String message) {
+		return message.toLowerCase();
+	}
+}
---
+++ rest-test/src/main/java/se/mawe75/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package se.mawe75.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestApplication extends Application {
+
+}
