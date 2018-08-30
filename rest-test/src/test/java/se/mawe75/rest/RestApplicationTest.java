2433b8c59f57e98be89c05b8f121261c94483fac
Magnus Westin <magnus.westin@xlent.se>
2018-08-30 14:25:34 +0200

    Added unit test

---
+++ rest-test/src/test/java/se/mawe75/rest/RestApplicationTest.java
@@ -0,0 +1,13 @@
+package se.mawe75.rest;
+
+import org.junit.Test;
+
+public class RestApplicationTest {
+
+	@Test
+	public void testRestApplicationConstructor() {
+		RestApplication a = new RestApplication();
+
+	}
+
+}
