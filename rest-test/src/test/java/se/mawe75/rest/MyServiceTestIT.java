3f56ec09b3f0199afa49b2be028a49d401ac8516
Magnus Westin <magnus.westin@xlent.se>
2018-08-31 12:52:10 +0200

    .

--- rest-test/src/test/java/se/mawe75/rest/MyServiceTestIT.java
+++ rest-test/src/test/java/se/mawe75/rest/MyServiceTestIT.java
@@ -4,18 +4,26 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyServiceTestIT {
+
+	private static final String DEFAULT_URL = "http://localhost:8181";
+	private String urlTest;
+
+	@BeforeClass
+	public void init() {
+		urlTest = System.getProperty("test.url", DEFAULT_URL) + "/rest/myservice";
+	}
 
 	@Test
 	public void test() {
 
 		Client client = ClientBuilder.newClient();
-		Response res = client.target("http://localhost:8181/rest/myservice").request("application/json").get();
+		Response res = client.target(urlTest).request("application/json").get();
 		System.out.println("respone: " + res);
-		
-		
+
 	}
 
 }
