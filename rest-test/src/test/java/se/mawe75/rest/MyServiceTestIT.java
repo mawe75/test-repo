f6a57f0624ba0bdb15811fa1e7b33196ae19c305
Magnus Westin <magnus.westin@xlent.se>
2018-08-31 13:02:04 +0200

    fixed IT-test

--- rest-test/src/test/java/se/mawe75/rest/MyServiceTestIT.java
+++ rest-test/src/test/java/se/mawe75/rest/MyServiceTestIT.java
@@ -10,18 +10,18 @@ import org.junit.Test;
 public class MyServiceTestIT {
 
 	private static final String DEFAULT_URL = "http://localhost:8181";
-	private String urlTest;
+	private static String TEST_SERVER_URL;
 
 	@BeforeClass
-	public void init() {
-		urlTest = System.getProperty("test.url", DEFAULT_URL) + "/rest/myservice";
+	public static void init() {
+		TEST_SERVER_URL = System.getProperty("test.url", DEFAULT_URL) + "/rest/myservice";
 	}
 
 	@Test
 	public void test() {
 
 		Client client = ClientBuilder.newClient();
-		Response res = client.target(urlTest).request("application/json").get();
+		Response res = client.target(TEST_SERVER_URL).request("application/json").get();
 		System.out.println("respone: " + res);
 
 	}
